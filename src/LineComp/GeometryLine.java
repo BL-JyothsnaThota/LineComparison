99e266a0e5ff1540eae496c7103fb04404012ac1
BL-JyothsnaThota <jyothsna.thota@bridgelabz.com>
2025-12-29 14:15:02 +0530

    Length of Line

--- src/LineComp/GeometryLine.java
+++ src/LineComp/GeometryLine.java
@@ -4,5 +4,10 @@ public class GeometryLine {
 
 	public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program");
+        Line line1 = new Line(2, 3, 5, 7);
+        double length = line1.calculateLength();
+
+        System.out.println("Length of the line: " + length);
+   
     }
 }
